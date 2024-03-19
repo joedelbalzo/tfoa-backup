b58d1a219e15cd02fa574e458d9ec6d9468ab38a
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-03-19 11:02:50 -0400

    css updates

--- Kathy/shaking tutrles.js
+++ Kathy/shaking tutrles.js
@@ -1,30 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var targetElements = document.querySelectorAll(".header-nav-wrapper > nav > div");
+const imageSources = [
+  "https://i.ibb.co/RjqygNH/Harley-shell-1.webp",
+  "https://i.ibb.co/gyfd6rq/Harley-shell-2.webp",
+  "https://i.ibb.co/5KKywV3/Harley-shell-3l.webp",
+];
 
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElements = document.querySelectorAll(".header-nav-wrapper > nav > div");
   targetElements.forEach(function (targetElement) {
-    var img = document.createElement("img");
-    img.src =
-      "https://images.squarespace-cdn.com/content/65c27dd45537d734bb9a0c4e/73179aaa-97bd-422a-a9d1-5243835f9a45/turtle.jpg?content-type=image%2Fjpeg";
+    const img = document.createElement("img");
+    if (targetElement == targetElements[0]) {
+      img.src = imageSources[0];
+    }
+    if (targetElement == targetElements[1]) {
+      img.src = imageSources[1];
+    }
+    if (targetElement == targetElements[2]) {
+      img.src = imageSources[2];
+    }
     img.style.position = "absolute";
-    img.style.top = "0";
-    img.style.left = "0";
-    img.style.width = "100%";
-    img.style.height = "100%";
+    /* img.style.top = '0';
+    img.style.left = '0'; */
+    img.style.width = "95%";
+    img.style.height = "160px";
+    img.style.filter = "drop-shadow(3px 3px 3px rgba(90,90,90,0.2))";
     img.style.zIndex = "-1";
-    img.style.objectFit = "cover";
-    img.classList.add("background-image");
+    img.style.objectFit = "contain";
 
     targetElement.style.position = "relative";
     targetElement.style.overflow = "hidden";
 
     targetElement.insertBefore(img, targetElement.firstChild);
 
-    img.addEventListener("mouseenter", function () {
-      this.style.animation = "jiggle 0.3s ease-in-out";
+    targetElement.addEventListener("mouseenter", function () {
+      img.classList.add("jiggle");
     });
-
-    img.addEventListener("mouseleave", function () {
-      this.style.animation = "none";
+    targetElement.addEventListener("mouseleave", function () {
+      img.classList.remove("jiggle");
     });
   });
 });
