00746b3a606d048c5bd4228c4ebdd24821c496aa
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-04-30 18:26:48 -0400

    kathy updates

--- Kathy/shaking tutrles.js
+++ Kathy/shaking tutrles.js
@@ -1,7 +1,13 @@
+{
+  /* <script> */
+}
+{
+  /* THIS IS FOR THE NAV TURTLES. PLEASE DO NOT TOUCH ANYTHING BETWEEN LINES 3 and 34!!*/
+}
 const imageSources = [
   "https://i.ibb.co/RjqygNH/Harley-shell-1.webp",
   "https://i.ibb.co/gyfd6rq/Harley-shell-2.webp",
-  "https://i.ibb.co/5KKywV3/Harley-shell-3l.webp",
+  "https://i.ibb.co/5KKywV3/Harley-shell-3l.webp" /*INSERT LINK FOR FOURTH IMAGE HERE*/,
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -17,20 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     if (targetElement == targetElements[2]) {
       img.src = imageSources[2];
     }
+    /* UNCOMMENT FOR FOURTH NAV IMAGE
+   if (targetElement == targetElements[3]){
+          img.src = imageSources[3];
+         }
+    */
     img.style.position = "absolute";
-    /* img.style.top = '0';
-    img.style.left = '0'; */
     img.style.width = "95%";
+    /* MAKE THE HEIGHT SMALLER THAN 160PX IF YOU'RE GOING TO HAVE MORE THAN THREE SHELLS */
     img.style.height = "160px";
     img.style.filter = "drop-shadow(3px 3px 3px rgba(90,90,90,0.2))";
     img.style.zIndex = "-1";
     img.style.objectFit = "contain";
-
     targetElement.style.position = "relative";
     targetElement.style.overflow = "hidden";
-
     targetElement.insertBefore(img, targetElement.firstChild);
-
     targetElement.addEventListener("mouseenter", function () {
       img.classList.add("jiggle");
     });
@@ -39,3 +46,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+{
+  /* /*END OF SHAKEY TURTLE NAVIGATION BAR CODE*/
+}
+
+{
+  /* </script> */
+}
